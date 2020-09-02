c2520078be69e0b5bea2af7afc0bb7c0e50bc5f7
ChieloNewctle <ChieloNewctle@Yandex.com>
2020-09-02 23:51:27 +0800

    add setup.py and cleanup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Copyright 2020 Jiang Shenghu
+# SPDX-License-Identifier: Apache-2.0
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyvlova',
+    version='0.1-dev',
+    description='A Simple Polyhedral Compiler for NN',
+    author='Jiang Shenghu',
+    author_email='ChieloNewctle@Yandex.com',
+    url='https://github.com/TheGreatCold/pyvlova',
+    packages=find_packages(),
+)
