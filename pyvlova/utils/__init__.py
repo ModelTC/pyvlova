9112e9d4b1bfeb0836ea09f95306785d894797f0
Chielo Newctle <ChieloNewctle@Yandex.com>
2021-05-24 22:04:52 +0800

    add customized function for transitive closure

--- pyvlova/utils/__init__.py
+++ pyvlova/utils/__init__.py
@@ -3,10 +3,13 @@
 from .utils import filter_contains, slugify, sizeof
 from .mode import Mode
 from .isl import get_unnamed_tuples, get_named_tuples, \
-    structure_named_fixed_box, structure_unnamed_fixed_box, map_out_constant_dim
-from .tir import tir_imm, tir_store, tir_load, tir_cuda_shared_sync, tir_thread_extent_attr
+    structure_named_fixed_box, structure_unnamed_fixed_box, \
+    map_out_constant_dim, transitive_closure
+from .tir import tir_imm, tir_store, tir_load, tir_cuda_shared_sync, \
+    tir_thread_extent_attr
 from .autotune import load_best
-from .sympy2isl import parse_sympy_to_isl_repr, constraints_to_isl_repr, ISLReprPrinter
+from .sympy2isl import parse_sympy_to_isl_repr, constraints_to_isl_repr, \
+    ISLReprPrinter
 
 
 def __get_cuda_settings():
--- pyvlova/utils/isl.py
+++ pyvlova/utils/isl.py
@@ -42,3 +42,47 @@ def map_out_constant_dim(obj):
     old, *_ = get_unnamed_tuples(obj)
     new = filter(lambda x: not x.isdigit(), old)
     return isl.map(f'{{ [{", ".join(old)}] -> [{", ".join(new)}] }}')
+
+
+isl.isl.isl_map_transitive_closure.restype = isl.c_void_p
+isl.isl.isl_map_transitive_closure.argtypes = [isl.c_void_p, isl.c_void_p]
+isl.isl.isl_union_map_transitive_closure.restype = isl.c_void_p
+isl.isl.isl_union_map_transitive_closure.argtypes = [isl.c_void_p, isl.c_void_p]
+
+
+def vanilla_isl_transitive_closure(umap):
+    umap = umap.coalesce()
+    if isinstance(umap, isl.map):
+        ptr = isl.isl.isl_map_copy(umap.ptr)
+        ptr = isl.isl.isl_map_transitive_closure(ptr, None)
+    else:
+        ptr = isl.isl.isl_union_map_copy(umap.ptr)
+        ptr = isl.isl.isl_union_map_transitive_closure(ptr, None)
+    return type(umap)(ctx=umap.ctx, ptr=ptr).coalesce()
+
+
+def binary_exp_transitive_closure(umap):
+    umap = umap.coalesce()
+    identity = umap.domain().identity()
+    if isinstance(umap, isl.map):
+        umap = umap.add_map(identity)
+    else:
+        umap = umap.union(identity)
+    umap = umap.coalesce()
+
+    for _ in range(64):
+        if umap.n_map() > 16 or len(str(umap)) > 4096:
+            return None
+        doubled = umap.apply_range(umap).coalesce()
+        if doubled.is_equal(umap):
+            return umap
+        umap = doubled
+
+    return None
+
+
+def transitive_closure(umap):
+    res = binary_exp_transitive_closure(umap)
+    if res is None:
+        return vanilla_isl_transitive_closure(umap)
+    return res
